18eb29ff7d5be5eb8c17aebb514cfa7f9ad6f87c
Darknight-cookie <tonytony0911@gmail.com>
2025-12-04 00:47:18 +0800

    Add period.cpp (string period practice)

---
+++ Algorithms/string/period.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int period() {
+    string str = "";
+    cin >> str;
+    int n = str.size();
+    for (int k = 1; k < n; k++) {
+        if (n % k != 0) continue;
+
+        bool check = true;
+        for (int i = k; i < n; i++) {
+            if (str[i] != str[i % k]) {
+                check = false;
+                break;
+            }
+        }
+        if (check)
+            return k;
+    }
+    return n;
+}
+int main()
+{
+    int num = 0;
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+        cout << "最小子字串長度：" << period() << endl;
+    }
+    
+
+}
+
